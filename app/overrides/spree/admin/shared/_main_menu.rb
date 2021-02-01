28461e24aefb35accc97cf4e395319e4d8922240
chinoxchen <chienfu.udp@gmail.com>
2021-01-31 22:09:35 -0300

    change icon

--- app/overrides/spree/admin/shared/_main_menu.rb
+++ app/overrides/spree/admin/shared/_main_menu.rb
@@ -5,7 +5,7 @@ Deface::Override.new(
   text:       <<-HTML
                 <% if current_spree_user.respond_to?(:has_spree_role?) && current_spree_user.has_spree_role?(:admin) %>
                   <ul class="nav nav-sidebar border-bottom">
-                    <%= tab plural_resource_name(Spree::Referal), url: admin_referals_path, icon: 'money' %>
+                    <%= tab plural_resource_name(Spree::Referal), url: admin_referals_path, icon: 'heart' %>
                   </ul>
                 <% end %>
 HTML
