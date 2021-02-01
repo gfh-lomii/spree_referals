e3ed430a84a473eef2a05a9ddd98cca98d26c5dc
chinoxchen <chienfu.udp@gmail.com>
2021-01-31 21:37:06 -0300

    fix error when no current order present

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -7,9 +7,14 @@ module SpreeReferals::Spree
     private
 
     def check_referal
-      return if params[:referal_id].blank?
-      referal = Spree::Referal.find_by(code: params[:referal_id])
+      return if params[:referal_id].blank? && cookies[:referal_id].blank?
+      referal_id = params[:referal_id] || cookies[:referal_id]
+      referal = Spree::Referal.find_by(code: referal_id)
       return if referal.blank?
+      cookies[:referal_id] = {value: referal_id, expires: Time.now + 4.hours}
+
+      return if current_order.blank?
+      return if current_order.spree_referal_id == referal.id
       current_order.update_column(:spree_referal_id, referal.id)
     end
 
